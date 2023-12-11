0675519a4e8f4d6c37b8f6826814d016b338fdb5
Burnaviour <meharmuzafar760@gmail.com>
2023-12-11 18:00:21 +0500

    Add logging and update commit message format

--- fb_upload.py
+++ fb_upload.py
@@ -1,33 +1,36 @@
 import os
 import datetime as dt
 import requests
+import logging
 from dotenv import load_dotenv
 from emoji import emojize
 
 load_dotenv()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 API_TOKEN = os.getenv("API_TOKEN")
 PAGE_ID = os.getenv("PAGE_ID")  # You need the Page ID to post to a specific page
-print(API_TOKEN)
+logger.info(API_TOKEN)
 date = dt.datetime.now().strftime("%d/%m/%Y")
 day = dt.datetime.now().strftime("%A")   
 alram=emojize(':alarm_clock:')
 shop=emojize(':shopping_cart:')
-
-message = f"Fortnite Item Shop {shop}\n{alram} {day}, {date}\nUse code 'BRNYT' to support me! #EpicPartner \n\n #fortnite #fortniteitemshop #fortniteitemshoplive #fortniteitemshopdaily #fortniteitemshopupdate #fortniteitemshopnow #fortniteitemshopnew #fortniteitemshopnews #fortniteitemsh\n"
+heart = emojize(':heartpulse:')
+message = f"Fortnite Item Shop {shop}\n{alram} {day}, {date}\n{heart} Use code 'BRNYT' to support me! #EpicPartner \n\n #fortnite #fortniteitemshop #fortniteitemshoplive #fortniteitemshopdaily #fortniteitemshopupdate #fortniteitemshopnow #fortniteitemshopnew #fortniteitemshopnews #fortniteitemsh\n"
 image_path = "final_image.jpeg"
 if API_TOKEN and PAGE_ID and os.path.isfile(image_path) and os.path.getsize(image_path) < 4 * 1024 * 1024:
-    print("Posting to Facebook...",PAGE_ID)
+    logger.info("Posting to Facebook... PAGE_ID: %s", PAGE_ID)
     url = f"https://graph.facebook.com/v18.0/{PAGE_ID}/photos"
     params = {"access_token": API_TOKEN, "message": message}
     with open(image_path, "rb") as image_file:
         files = {"source": image_file}
         try:
-            print("Uploading image...", image_file)
+            logger.info("Uploading image... %s", image_file)
             response = requests.post(url, params=params, files=files)
-            print(response.json())
+            logger.info(response.json())
         except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
+            logger.error("Error: %s", e)
 else:
-    print("Invalid image file or missing API_TOKEN or PAGE_ID.")
+    logger.error("Invalid image file or missing API_TOKEN or PAGE_ID.")
